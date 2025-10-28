e23cf3168443f386b6099cd5fb86c23dbe04efa5
tabrezsyed1998 <tabrezsyed1998@gmail.com>
2025-10-28 19:24:44 +0530

    test cases changes

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import styles from "./Logo.module.css"
 
-const Logo = () => <img src={logo} alt="logo" />;
+const Logo = () => <img src={logo} alt="logo" className={styles.logo}/>;
 
 export default Logo;
